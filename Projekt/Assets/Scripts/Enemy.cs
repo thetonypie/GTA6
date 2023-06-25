b06c2da138368c4bc6794efcf946f804f0a1053e
Pdev <122171858+Pdevel@users.noreply.github.com>
2023-06-25 19:15:37 +0200

    Poprawa pistoletu

--- Projekt/Assets/Scripts/Enemy.cs
+++ Projekt/Assets/Scripts/Enemy.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int maxHealth = 100; // Maksymalne zdrowie
+    private int currentHealth; // Obecne zdrowie
+
+    private Renderer renderer;
+    private Color originalColor;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
+
     public void AddDamage(int damage)
     {
-        Debug.Log("Ouch, " + damage);
+        currentHealth -= damage;
+        StartCoroutine(FlashColor(Color.red, 0.1f)); // Zmiana koloru na czerwony
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject); // Zniszczenie obiektu, gdy zdrowie osi¹gnie 0
+        }
+    }
+
+    IEnumerator FlashColor(Color color, float duration)
+    {
+        renderer.material.color = color;
+        yield return new WaitForSeconds(duration);
+        renderer.material.color = originalColor;
     }
 }
